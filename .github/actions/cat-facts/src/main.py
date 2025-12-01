a3093051f9a149e99759d1a2023ba690f032c1ba
johnhain_microsoft <johnhain@microsoft.com>
2025-12-01 15:46:50 -0500

    FIX: reset lab-5 to defaults

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,17 +1,29 @@
 import requests
-import os
+import random
+import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://catfact.ninja/fact"
+cat_url = "https://cat-fact.herokuapp.com/facts"
 r = requests.get(cat_url)
-r_obj = r.json()
+r_obj_list = r.json()
 
-# Get the fact directly from the response
-random_fact = r_obj["fact"]
+# Create an empty list to store individual facts in
+# This will make it easy to select a random one later
+fact_list = []
 
-# Print the cat fact
+# Add the "text" of every object into the fact_list list
+for fact in r_obj_list:
+    fact_list.append(fact["text"])
+
+# Select a random fact from the fact_list and return it
+# into a variable named random_fact so we can use it
+def select_random_fact(fact_arr):
+    return fact_arr[random.randint(0, len(fact_list)+1)]
+
+random_fact = select_random_fact(fact_list)
+
+# Print the individual randomly returned cat-fact
 print(random_fact)
 
-# Set the fact output of the action using GITHUB_OUTPUT
-with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
-    f.write(f"fact={random_fact}\n")
+# Set the fact-output of the action as the value of random_fact
+print(f"fact={random_fact}")
